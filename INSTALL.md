62a890553f9130233966b8a10d27d145d41d7847
Bislinks Dotcom <github@bislinks.com>
2022-01-20 08:33:13 -0600

    008 published to github:/bislink/ping-dig. Modified License/Install.

---
+++ INSTALL.md
@@ -0,0 +1,14 @@
+# ping/dig for your domain.
+
+## Usage
+
+### Virtualmin
+Go to Webmin -> Tools -> Custom Commands':
+  copy/paste ping-dig.sh; 
+    Change domain name on line 4
+    Change nsN to whatever name server your domain is server from
+    Removed ones that do not apply or not necessary.
+
+### CMD Line
+
+./ping-dash.sh
--- ping-dig.sh
+++ ping-dig.sh
@@ -1,5 +1,4 @@
-echo "Ping/Dig Version #0.0.7";
-echo "To be used as 'custom command in Virtualmin : Webmin -> Tools -> Custom Commands': just change domain name!"
+echo "Ping/Dig Version #0.0.8";
 echo "";
 
 DOMAIN="bislinks.com";
@@ -9,66 +8,93 @@ echo "";
 echo "$DOMAIN";
 ping -c1 -4 $DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
 echo "ns1.$DOMAIN";
 ping -c1 -4 ns1.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
+echo "ns2.$DOMAIN";
+ping -c1 -4 ns2.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
+echo "";
+
 echo "ns3.$DOMAIN";
 ping -c1 -4 ns3.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
 echo "ns4.$DOMAIN";
 ping -c1 -4 ns4.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
 echo "ns5.$DOMAIN";
 ping -c1 -4 ns5.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
+echo "ns6.$DOMAIN";
+ping -c1 -4 ns6.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
+echo "";
+
 echo "ns7.$DOMAIN";
 ping -c1 -4 ns7.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
+
 echo "ns40.$DOMAIN";
 ping -c1 -4 ns40.$DOMAIN | head -n1 | cut -d"(" -f2 | cut -d")" -f1;
 echo "";
 
 echo "DIG";
 echo "";
+
 echo "NS";
 dig +short $DOMAIN ns;
 echo "";
+
 echo "@ns1 A";
 dig +short @ns1.$DOMAIN $DOMAIN a;
 echo "";
+
 echo "@ns3 A";
 dig +short @ns3.$DOMAIN $DOMAIN a;
 echo "";
+
 echo "@ns4 A";
 dig +short @ns4.$DOMAIN $DOMAIN a;
 echo "";
+
 echo "@ns5 A";
 dig +short @ns5.$DOMAIN $DOMAIN a;
 echo "";
+
 echo "@ns7 A";
 dig +short @ns7.$DOMAIN $DOMAIN a;
 echo "";
+
 echo "@ns40 A";
 dig +short @ns40.$DOMAIN $DOMAIN a;
 echo "";
 
 echo "DIG SOA";
 echo "";
+
 echo "@ns1 SOA";
 dig +short @ns1.$DOMAIN $DOMAIN soa;
 echo "";
+
 echo "@ns3 SOA";
 dig +short @ns3.$DOMAIN $DOMAIN soa;
 echo "";
+
 echo "@ns4 SOA";
 dig +short @ns4.$DOMAIN $DOMAIN soa;
 echo "";
+
 echo "@ns5 SOA";
 dig +short @ns5.$DOMAIN $DOMAIN soa;
 echo "";
+
 echo "@ns7 SOA";
 dig +short @ns7.$DOMAIN $DOMAIN soa;
 echo "";
+
 echo "@ns40 SOA";
 dig +short @ns40.$DOMAIN $DOMAIN soa;
 echo "";
